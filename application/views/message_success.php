241a14b89bbfeebd791fe8246258d9ed6cb4aa3a
RicardoSuarez <ing.ricadosuarezc@gmail.com>
2016-12-27 06:50:22 -0500

    Se modifica la pagina de message_success

--- application/views/message_success.php
+++ application/views/message_success.php
@@ -1,4 +1,5 @@
-
+<section class="main container">
 <div class="alert alert-success" role="alert">
   Felicitaciones <strong><?php echo $nom_part ?> <?php echo $ape_part ?>,</strong> te has inscrito en la <strong>Carrera Internacionl San Silvestre,</strong> puedes consultar tu inscripción con tu número de identificación <b><?php echo $num_ident_part ?></b> en el siguiente link. <a href="<?php echo base_url();?>participantes/consultaInscripcion">Consultar</a>
 </div>
+</section>
